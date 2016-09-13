56be55d1eaf06b95f8d9b690fe47ed29dfd62e88
keenan <keenanphillips@gmail.com>
2016-09-13 12:06:10 -0700

    got walls added and collision detection but haven't figured out how to block movement in advance of a collision

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 
+// var loader = new THREE.TextureLoader();
+// var wallTexture = loader.load('http://i.imgur.com/Mtgockn.jpg', );
+// var texture2 = loader.load('texture2.jpg');
+
 //Initial setup
   let scene = new THREE.Scene();
   let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   let renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
+  let collidableMeshList = []
 
 // Meshes
   let cubeGeometry = new THREE.BoxGeometry( 1, 1, 1 );
@@ -13,12 +18,16 @@
   let villainMaterial = new THREE.MeshLambertMaterial( { color: 0xCC0000 } );
   let bulletMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
   let cube = new THREE.Mesh( cubeGeometry, heroMaterial );
-  let cube2 = new THREE.Mesh( cubeGeometry, villainMaterial );
+  let villain = new THREE.Mesh( cubeGeometry, villainMaterial );
   let rightWall = new THREE.Mesh()
-  scene.add(cube2)
+
+  scene.add(villain)
+  collidableMeshList.push(villain)
+
   let spheres = []
   let bulletYAngles = []
   cube.position.x = 1
+  villain.position.y = 0.5
   scene.add( cube );
 // create light sources
   let pointLight1 = new THREE.PointLight(0xFFFFFF);
@@ -28,10 +37,9 @@
   scene.add(pointLight1);
   scene.add(ambientLight);
 
-  window.Scene = scene
 
 // Camera and Render
-  camera.position.z = 3;
+  camera.position.z = 1;
   camera.position.y = 1
   camera.position.x = 0
   cube.add(camera)
@@ -39,13 +47,14 @@
     requestAnimationFrame( render );
 
 
-    cube2.rotation.x += 0.01;
-    cube2.rotation.y += 0.01;
+    villain.rotation.x += 0.01;
+    villain.rotation.y += 0.01;
 
     spheres.forEach((sphere, index) => {
       sphere.position.x -= Math.sin(bulletYAngles[index])
       sphere.position.z -= Math.cos(bulletYAngles[index])
     })
+    detectCollision()
     renderer.render( scene, camera );
   }
   render();
@@ -56,14 +65,13 @@
 
     if (key === 37) {
       cube.rotation.y += 0.1
-      console.log("y rot", cube.rotation.y);
     } else if (key === 38) {
-      cube.translateZ(-0.1)
+      if(cube.translateZ(-0.3))
+      cube.translateZ(-0.3)
     } else if (key === 39){
       cube.rotation.y -= 0.1
-      console.log(cube.rotation.y);
     } else if (key === 40){
-      cube.translateZ(0.1)
+      cube.translateZ(0.3)
     }
     if (key === 32){
       let sphere = new THREE.Mesh(sphereGeometry, bulletMaterial);
@@ -81,19 +89,22 @@
     }
   }
 
+const mapWidth = 50
+const mapHeight = 5
+
 function setupMap (){
   let map = [ // 1  2  3  4  5  6  7  8  9
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 0
-             [1, 0, 0, 0, 0, 0, 0, 1, 1, 1,], // 1
+             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 1
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 2
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 3
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 4
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 5
-             [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 6
+             [1, 0, 0, 1, 1, 1, 1, 0, 0, 1,], // 3
+             [1, 0, 0, 1, 0, 0, 1, 0, 0, 1,], // 4
+             [1, 0, 0, 1, 0, 0, 1, 0, 0, 1,], // 5
+             [1, 0, 0, 0, 0, 1, 1, 0, 0, 1,], // 6
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 7
              [1, 0, 0, 0, 0, 0, 0, 0, 0, 1,], // 8
              [1, 1, 1, 1, 1, 1, 1, 1, 1, 1,], // 9
-           ], mapWidth = map.length * 5, mapHeight = 3;
+           ]
 
   let floor = new THREE.Mesh(
     new THREE.CubeGeometry(mapWidth, 0.0001, mapWidth),
@@ -102,7 +113,14 @@ function setupMap (){
   floor.position.y = -0.5
   scene.add(floor)
 
-  let wallCube = new THREE.CubeGeometry(1, mapHeight, 1);
+  let ceiling = new THREE.Mesh(
+    new THREE.CubeGeometry(mapWidth, 0.0001, mapWidth),
+    new THREE.MeshLambertMaterial({color: 0x990099})
+  )
+  ceiling.position.y = 4
+  scene.add(ceiling)
+
+  let wallCube = new THREE.CubeGeometry(5, mapHeight, 5);
 
   let wallMaterial = new THREE.MeshLambertMaterial({color: 0xCC99CC})
   // [
@@ -110,18 +128,43 @@ function setupMap (){
   //   new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('assets/images/wall.jpg')})
   // ]
 
-  for(let i = 0; i < mapWidth; i++) {
-    for(let j = 0; j < map[i].length; j++){
-      let wall = new THREE.Mesh(wallCube, wallMaterial);
-      debugger
-      wall.positiion.set(
-        ((j * 5) - 5),
-        ((i * 5) - 5),
-        (-0.5)
-      );
-      scene.add(wall)
+  for(let i = 0; i < map.length; i++) {
+    for(let j = 0, length = map[i].length; j < length; j++){
+      if  (map[i][j]){
+        let wall = new THREE.Mesh(wallCube, wallMaterial);
+        wall.position.set((j - 5) * 5, 2, (i - 5) * 5)
+        scene.add(wall)
+        collidableMeshList.push(wall)
+      }
     }
   }
 }
 
 setupMap()
+
+function detectCollision(){
+  for (var vertexIndex = 0; vertexIndex < cube.geometry.vertices.length; vertexIndex++)
+  {
+    var localVertex = cube.geometry.vertices[vertexIndex].clone();
+    // var globalVertex = cube.matrix.multiplyVector3(localVertex);
+    var globalVertex = localVertex.applyMatrix4(cube.matrix);
+    var directionVector = globalVertex.sub( cube.position );
+
+    var ray = new THREE.Raycaster( cube.position, directionVector.clone().normalize() );
+    var collisionResults = ray.intersectObjects( collidableMeshList );
+    if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
+    {
+      // a collision occurred... do something...
+      return true;
+    }
+  }
+}
+
+
+
+
+
+
+
+window.Scene = scene
+window.Cube = cube
