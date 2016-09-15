dfae03269999dc2377a373524cc09e68f24f363c
keenan <keenanphillips@gmail.com>
2016-09-15 12:03:08 -0700

    got villains firing and health decrimenting

--- public/js/main.js
+++ public/js/main.js
@@ -1,182 +1,195 @@
 
-
 var loader = new THREE.TextureLoader();
-// loader.crossOrigin = ""
-// var wallTexture = loader.load('http://i.imgur.com/Mtgockn.jpg', );
-// var texture2 = loader.load('texture2.jpg');
-
 //Initial setup
-  let scene = new THREE.Scene();
-  let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-  let renderer = new THREE.WebGLRenderer();
-  renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-  // let collidableMeshList = []
-  let villains = []
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+let villains = []
 
 // Meshes
-  let cubeGeometry = new THREE.BoxGeometry( 0.5, 0.5, 1 )
-  let sphereGeometry = new THREE.IcosahedronGeometry(0.25, 2);
-  let heroMaterial = new THREE.MeshLambertMaterial( { color: 0x336699 } );
-  let bulletMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
-  let cube = new THREE.Mesh( cubeGeometry, heroMaterial );
-  // let villain = new THREE.Mesh( villainGeometry, villainMaterial );
-  let rightWall = new THREE.Mesh()
+let cubeGeometry = new THREE.BoxGeometry( 0.5, 0.5, 1 )
+let sphereGeometry = new THREE.IcosahedronGeometry(0.25, 4);
+let heroMaterial = new THREE.MeshLambertMaterial( { color: 0x336699 } );
+let bulletMaterial = new THREE.MeshNormalMaterial( { wireframe: true } );
+let cube = new THREE.Mesh( cubeGeometry, heroMaterial );
+cube.health = 100
 
-  function setupVillains(number){
-    let villainGeometry = new THREE.BoxGeometry( 1.5, 1.5, 1.5 )
-    for(let i = 0; i < number; i++){
-      let villainMaterial = new THREE.MeshLambertMaterial( { color: 0xCC0000 } );
-      let villain = new THREE.Mesh( villainGeometry, villainMaterial );
-      villain.position.x = Math.random() * mapWidth/2
-      villain.position.z = Math.random() * mapWidth/2
-      villain.hits = 0
-      scene.add(villain)
-      villains.push(villain)
-    }
+
+function setupVillains(number){
+  let villainGeometry = new THREE.IcosahedronGeometry( 1.25, 3 )
+  for(let i = 0; i < number; i++){
+    let villainTexture = loader.load('/images/webs.jpg');
+    // villainTexture.offset(0.5)
+    let villainMaterial = new THREE.MeshLambertMaterial( { map: villainTexture } );
+    let villain = new THREE.Mesh( villainGeometry, villainMaterial );
+    villain.position.x = Math.random() * 22
+    villain.position.z = Math.random() * 22
+    villain.hits = 0
+    villain.shooting = false
+    villain.countTimer = 0
+    scene.add(villain)
+    villains.push(villain)
   }
+}
 
-  // scene.add(villain)
+// scene.add(villain)
 
-  let spheres = []
-  let bulletYAngles = []
-  cube.position.x = -20
-  cube.position.z = 20
-  // villain.position.y = 0.5
-  // villain.hits = 0
-  // villains.push(villain)
+let spheres = []
+let evilSpheres = []
+let evilTrajectories = []
+let bulletYAngles = []
+cube.position.x = -20
+cube.position.z = 20
 
-  scene.add( cube );
+
+scene.add( cube );
+
 // create light sources
-  let pointLight1 = new THREE.PointLight(0xFFFFFF);
-  let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.5);
+let pointLight1 = new THREE.PointLight(0xFFFFFF);
+let ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.7);
 
-  pointLight1.position.set(10, 50, -130)
-  scene.add(pointLight1);
-  scene.add(ambientLight);
-  scene.fog = new THREE.FogExp2(0xD6F1FF, 0.01);
+pointLight1.position.set(10, 50, -130)
+scene.add(pointLight1);
+scene.add(ambientLight);
+scene.fog = new THREE.FogExp2(0xD6F1FF, 0.01);
 
 
 // Camera and Render
-  camera.position.z = 1;
-  camera.position.y = 1
-  camera.position.x = 0
-  cube.add(camera)
+camera.position.z = 1;
+camera.position.y = 1
+camera.position.x = 0
+cube.add(camera)
 
-  function render() {
-    requestAnimationFrame( render );
+function render() {
+  requestAnimationFrame( render );
 
-    spheres.forEach((sphere, index) => {
-      sphere.position.x -= Math.sin(bulletYAngles[index])
-      sphere.position.z -= Math.cos(bulletYAngles[index])
-      if(Math.abs(sphere.position.x > 25) || Math.abs(sphere.position.z > 25)) {
-        scene.remove(sphere);
-      }
-      villains.forEach((villain) => {
-        if(detectCollision(villain, sphere)){
-          villain.hits += 1
-          if(villain.hits > 10){
-            scene.remove(villain)
-          } else {
-            villain.material.color.setRGB(
-              1/(villain.hits/4) * villain.material.color.r,
-              1/(villain.hits/5) * villain.material.color.g,
-              1/(villain.hits/5) * villain.material.color.b
-            )
-          }
-        }
-      })
-    })
-
+  spheres.forEach((sphere, index) => {
+    sphere.position.x -= Math.sin(bulletYAngles[index])
+    sphere.position.z -= Math.cos(bulletYAngles[index])
     villains.forEach((villain) => {
-      if(Math.abs(villain.position.x) > 21 || Math.abs(villain.position.z) > 21){
-        resetPosition(villain)
-        villain.rotation.y += Math.random() * Math.PI
-        // let ninety = Math.PI/2
-        // let randomAngleUnder90 = Math.random() * ninety
-        // if(villain.position.x > 0 && villain.position.z > 0){//
-        //   villain.rotation.y = (randomAngleUnder90) + ninety
-        // } else if(villain.position.x > 0 && villain.position.z < 0){//getting stuck between this corner
-        //   villain.rotation.y = (randomAngleUnder90)
-        // } else if(villain.position.x < 0 && villain.position.z > 0){
-        //   villain.rotation.y = (randomAngleUnder90) + ninety
-        // } else if(villain.position.x < 0 && villain.position.z < 0){//getting stuck between this corner
-        //   villain.rotation.y = (randomAngleUnder90) + (2 * ninety)
-        // }
+      if(detectCollision(villain, sphere)){
+        villain.hits += 1
+        if(villain.hits > 10){
+          scene.remove(villain)
+          villains.splice(villains.indexOf(villain), 1)
+          villain = null
+        } else {
+          villain.material.color.setRGB(
+            1/(villain.hits/4) * villain.material.color.r,
+            1/(villain.hits/5) * villain.material.color.g,
+            1/(villain.hits/5) * villain.material.color.b
+          )
+        }
       }
-
-      villain.translateX(0.05)
     })
-    // detectCollision()
-    renderer.render( scene, camera );
-  }
-
-  function resetPosition(object){
-    if(object.position.x > 21) {object.position.x -= 0.05}
-    if(object.position.z > 21) {object.position.z -= 0.05}
-    if(object.position.x < 21) {object.position.x += 0.05}
-    if(object.position.z < 21) {object.position.z += 0.05}
-  }
-// Key Listeners for controls
-  window.onkeydown = (e) => {
-    let key = e.keyCode ? e.keyCode : e.which;
-
-    if (key === 37) {
-      cube.rotation.y += 0.1
-    } else if (key === 38) {
-      // if(cube.translateZ(-0.3))
-      if (Math.abs(cube.position.x) <= 22) {
-        cube.position.x -= Math.sin(cube.rotation.y)/2
-      } else if (cube.position.x >= 22) {
-        cube.position.x = 21.99999999;
-      } else if (cube.position.x <= 22) {
-        cube.position.x = -21.99999999;
-      }
-
-      if (Math.abs(cube.position.z) <= 22) {
-        cube.position.z -= Math.cos(cube.rotation.y)/2
-      } else if (cube.position.z >= 22) {
-        cube.position.z = 21.99999999;
-      } else if (cube.position.z <= 22) {
-        cube.position.z = -21.99999999;
-      }
-
-      // cube.position.x -= Math.sin(cube.rotation.y)/2
-      // cube.position.z -= Math.cos(cube.rotation.y)/2
-      // cube.translateZ(-0.3)
-    } else if (key === 39){
-      cube.rotation.y -= 0.1
-    } else if (key === 40){
-      if (Math.abs(cube.position.x) < 22) {
-        cube.position.x += Math.sin(cube.rotation.y)/2
-      } else if (cube.position.x >= 22) {
-        cube.position.x = 21.99999999;
-      } else if (cube.position.x <= -22) {
-        cube.position.x = -21.99999999;
-      }
-
-      if (Math.abs(cube.position.z) < 22) {
-        cube.position.z += Math.cos(cube.rotation.y)/2
-      } else if (cube.position.z >= 22) {
-        cube.position.z = 21.99999999;
-      } else if (cube.position.z <= -22) {
-        cube.position.z = -21.99999999;
-      }
-      // cube.translateZ(0.3)
+    if(Math.abs(sphere.position.x > 25) || Math.abs(sphere.position.z > 25)) {
+      scene.remove(sphere);
+      spheres.splice(index, 1)
+      bulletYAngles.splice(index, 1)
+      sphere = null
     }
-    if (key === 32){
-      let sphere = new THREE.Mesh(sphereGeometry, bulletMaterial);
-      sphere.position.set(cube.position.x, cube.position.y, cube.position.z)
-      sphere.rotation.set(3.28, 3.28, 3.28)
-
-      scene.add(sphere)
-      // sphere.rotation.set(cube.rotation.x, cube.rotation.y, cube.rotation.z)
-      spheres.push(sphere)
-      // collidableMeshList.push(sphere)
-
-      bulletYAngles.push(cube.rotation.y)
+  })
+  evilSpheres.forEach((sphere, index) => {
+    sphere.position.x -= Math.sin(evilTrajectories[index])
+    sphere.position.z -= Math.cos(evilTrajectories[index])
+    if(detectCollision(cube, sphere)){
+      cube.health -= 1
+      manageHit()
     }
+  })
+
+  function manageHit(){
+    document.getElementById("health").innerHTML = cube.health
   }
+
+  villains.forEach((villain) => {
+    if(Math.abs(villain.position.x) > 21 || Math.abs(villain.position.z) > 21){
+      resetPosition(villain)
+      villain.rotation.y += Math.random() * Math.PI
+    }
+    if(distance(villain.position.x, villain.position.z, cube.position.x, cube.position.z) < 10){
+      // villain.shooting = true
+      villain.countTimer +=1
+      if(villain.countTimer % 140 === 0){
+        let villainSphere = new THREE.Mesh(sphereGeometry, bulletMaterial);
+        villainSphere.position.set(villain.position.x, villain.position.y, villain.position.z)
+        let trajectory = Math.atan2(-(cube.position.x - villain.position.x), -(cube.position.z - villain.position.z)) + ((Math.random() - 0.5) * 0.2)
+        villain.rotation.y = trajectory - Math.PI/2
+        scene.add(villainSphere)
+        evilSpheres.push(villainSphere)
+        evilTrajectories.push(trajectory)
+      }
+    } else {
+      villain.translateX(-0.05)
+    }
+  })
+  renderer.render( scene, camera );
+}
+
+function resetPosition(object){
+  if(object.position.x > 21) {object.position.x -= 0.05}
+  if(object.position.z > 21) {object.position.z -= 0.05}
+  if(object.position.x < 21) {object.position.x += 0.05}
+  if(object.position.z < 21) {object.position.z += 0.05}
+}
+
+
+
+window.onkeydown = (e) => {
+  let key = e.keyCode ? e.keyCode : e.which;
+
+  if (key === 37) {
+    cube.rotation.y += 0.1
+  } else if (key === 38) {
+    if (Math.abs(cube.position.x) <= 22) {
+      cube.position.x -= Math.sin(cube.rotation.y)/2
+    } else if (cube.position.x >= 22) {
+      cube.position.x = 21.99999999;
+    } else if (cube.position.x <= 22) {
+      cube.position.x = -21.99999999;
+    }
+
+    if (Math.abs(cube.position.z) <= 22) {
+      cube.position.z -= Math.cos(cube.rotation.y)/2
+    } else if (cube.position.z >= 22) {
+      cube.position.z = 21.99999999;
+    } else if (cube.position.z <= 22) {
+      cube.position.z = -21.99999999;
+    }
+
+  } else if (key === 39){
+    cube.rotation.y -= 0.1
+  } else if (key === 40){
+    if (Math.abs(cube.position.x) < 22) {
+      cube.position.x += Math.sin(cube.rotation.y)/2
+    } else if (cube.position.x >= 22) {
+      cube.position.x = 21.99999999;
+    } else if (cube.position.x <= -22) {
+      cube.position.x = -21.99999999;
+    }
+
+    if (Math.abs(cube.position.z) < 22) {
+      cube.position.z += Math.cos(cube.rotation.y)/2
+    } else if (cube.position.z >= 22) {
+      cube.position.z = 21.99999999;
+    } else if (cube.position.z <= -22) {
+      cube.position.z = -21.99999999;
+    }
+    // cube.translateZ(0.3)
+  }
+  if (key === 32){
+    let sphere = new THREE.Mesh(sphereGeometry, bulletMaterial);
+    sphere.position.set(cube.position.x, cube.position.y, cube.position.z)
+    sphere.rotation.set(3.28, 3.28, 3.28)
+
+    scene.add(sphere)
+    spheres.push(sphere)
+
+    bulletYAngles.push(cube.rotation.y)
+  }
+}
 
 const mapWidth = 50
 const mapHeight = 5
@@ -205,10 +218,6 @@ function setupMap (){
 
   var wallTexture = loader.load('/images/spaceshipTexture.jpg');
   let wallMaterial = new THREE.MeshLambertMaterial({map: wallTexture})
-  // [
-  //   null,
-  //   new THREE.MeshLambertMaterial({map: THREE.ImageUtils.loadTexture('assets/images/wall.jpg')})
-  // ]http://previews.123rf.com/images/kentoh/kentoh1103/kentoh110300048/8971306-Seamless-Space-Hull-Ship-Pattern-as-Background-Stock-Photo-spaceship-texture.jpg
 
   for(let i = 0; i < map.length; i++) {
     for(let j = 0, length = map[i].length; j < length; j++){
@@ -216,14 +225,16 @@ function setupMap (){
         let wall = new THREE.Mesh(wallCubes[map[i][j]], wallMaterial);
         wall.position.set((i - 5) * 5, 2, (j - 5) * 5)
         scene.add(wall)
-        // collidableMeshList.push(wall)
       }
     }
   }
 
+  var floorTexture = loader.load('/images/floorAlt.jpg');
+  floorTexture.wrapS = floorTexture.wrapT = THREE.RepeatWrapping;
+  floorTexture.repeat.set(5,10)
   let floor = new THREE.Mesh(
     new THREE.CubeGeometry(mapWidth, 0.0001, mapWidth),
-    wallMaterial//new THREE.MeshLambertMaterial({color: 0x666666})
+    new THREE.MeshLambertMaterial({map: floorTexture })
   )
   floor.position.y = -0.5
   scene.add(floor)
@@ -231,7 +242,7 @@ function setupMap (){
   var ceilingTexture = loader.load('/images/newspace.png');
   let ceiling = new THREE.Mesh(
     new THREE.CubeGeometry(mapWidth, 0.0001, mapWidth),
-    new THREE.MeshLambertMaterial({map: ceilingTexture})
+    new THREE.MeshLambertMaterial({map: ceilingTexture })
   )
   ceiling.position.y = 4
   scene.add(ceiling)
@@ -244,35 +255,10 @@ setupMap()
 function distance(x1, y1, x2, y2) {
 	return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
 }
-// function detectCollision(){
-//   for (var vertexIndex = 0; vertexIndex < villain.geometry.vertices.length; vertexIndex++)
-//   {
-//     var localVertex = villain.geometry.vertices[vertexIndex].clone();
-//     // var globalVertex = villain.matrix.multiplyVector3(localVertex);
-//     var globalVertex = localVertex.applyMatrix4(villain.matrix);
-//     var directionVector = globalVertex.sub( villain.position );
-//
-//     var ray = new THREE.Raycaster( villain.position, directionVector.clone().normalize() );
-//     var collisionResults = ray.intersectObjects( collidableMeshList );
-//     if ( collisionResults.length > 0 && collisionResults[0].distance < directionVector.length() )
-//     {
-//       // a collision occurred... do something...
-//       console.log("hit!!!");
-//     }
-//   }
-// }
 
 function detectCollision(object1, object2){
   return (Math.abs(object1.position.x - object2.position.x) < 1) &&
   (Math.abs(object1.position.z - object2.position.z) < 1)
 }
 
-// $(document).ready(() => {
   render();
-// })
-
-
-
-
-window.Scene = scene
-window.Cube = cube
